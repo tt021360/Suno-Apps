3e1e840679ddcba724f60e4f258e748f6a275434
Ted Tracy <ted.tracy@gmail.com>
2023-06-28 16:55:36 -0700

    Added real code example.
    Made first working program.

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,13 @@
+mylist = [1,2,3,4,5,6,7,8,9,10]
+
 print("Hello, World")
 print("Adding a second line commit...")
+
+for num in mylist:
+	#check even vs odd
+	if num % 2 == 0:
+		print("Even")
+	else:
+		print("Odd")
+	print (num)
+print("All done")		
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Suno-Apps
 Repository to hold apps for the upcoming Suno demonstrations.
 My first set of edits to understand how to use the repository for the demo.
+Adding a third innocuous comment.
